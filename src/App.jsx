10a51188ebe27a3754a98ddc33583d0f9b1ba59b
Dario Lopez <dlopez@kooomo.com>
2024-08-01 10:11:44 +0200

    215 - state batching

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,13 @@ function App() {
 
   function handleSetCount(newCount) {
     setChosenCount(newCount);
+    setChosenCount((prevChosenCount) => prevChosenCount + 1); //these two state updates will not
+    //trigger two component function executions. Instead, React will perform a State Batching
+    //which will mainly update the state as many times as the same function is called (setChousenCount in this case)
+    //and after that the component function is executed
+    console.log(chosenCount); //this line here will not print the update state but the old value of it
+    //the reason for this is the state update is scheduled by React. It will happen pretty much
+    //instantly but the line after the state update will still not get the new value of it.
   }
 
   return (
